6ef2104c1a3a6162c811589e66a55f02f2f8455b
Marc O'Morain <marc@flipdish.com>
2023-12-16 22:23:30 +0000

    Do day 4

---
+++ day04/core.clj
@@ -0,0 +1,77 @@
+(ns day04.core
+  (:require [clojure.string :as s]
+            [clojure.set :as set]
+            [clojure.math :as math]))
+
+(defn parse-ints [s]
+  (->>
+   (s/split s #"\s+")
+   (filter (complement s/blank?))
+   (mapv s/trim)
+   (mapv parse-long)))
+
+(parse-ints "  12 2   2   2")
+(defn parse [line]
+  (let [[_ card win have] (re-matches #"Card +(\d+):([ \d]+)\|([ \d]+)" line)]
+    {:card (parse-long card)
+     :win (set (parse-ints win))
+     :have (set (parse-ints have))}))
+
+(parse "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+
+(defn points [n]
+  (cond
+    (= 0 n) 0
+    (= 1 n) 1
+    :else (long (math/pow 2 (dec n)))))
+
+(points 4)
+
+(defn associate-by [f coll]
+  (zipmap (mapv f coll) coll))
+
+(defn score [{:keys [card win have] :as c}]
+  (let [matches (set/intersection win have)]
+    (assoc c
+           :matches matches
+           :points (points (count matches))
+           :awards (set (range (inc card) (+ (inc card) (count matches)))))))
+
+
+(defn compute-worth [card others]
+  (let [c->card (associate-by :card others)
+        wins (select-keys c->card (:awards card))
+        points (apply + (mapv :worth (vals wins)))]
+    (inc points)))
+
+
+(compute-worth
+ {:card 4, :win #{69 92 41 73 84}, :have #{59 58 54 51 76 5 83 84}, :matches #{84}, :points 1, :awards #{5}}
+ [{:card 5, :win #{32 28 83 26 87}, :have #{70 88 22 36 93 12 82 30}, :matches #{}, :points 0, :awards #{} :worth 1}
+  {:card 6, :win #{72 31 56 13 18}, :have #{74 77 36 23 35 11 10 67}, :matches #{}, :points 0, :awards #{} :worth 1}])
+
+
+(defn part2 [scores]
+  (loop [[head & tail] (reverse scores)
+         result []]
+    (if-not (some? head)
+      result
+      (let [worth (compute-worth head result)
+            card-with-worth (assoc head :worth worth)]
+        (recur tail (conj result card-with-worth))))))
+
+(defn part1 [scores]
+  (apply + (mapv :points scores)))
+
+(defn wins [path]
+  (let [data (slurp path)
+        lines (s/split-lines data)
+        cards (mapv parse lines)
+        scores (mapv score cards)]
+    (->> scores
+         (part2)
+         (map :worth)
+         (apply +))))
+
+(wins "day04/example.txt")
+(wins "day04/input.txt")
--- main.clj
+++ main.clj
@@ -1,4 +1,6 @@
-(require '[clojure.string :as s])
+(ns main
+  (:require [clojure.string :as s]
+            [clojure.pprint :refer [pprint]]))
 
 (def stems
   [["0" 0]
@@ -92,14 +94,14 @@
 (possible? ["1" [{"red" 12} {"blue" 111}]])
 
 
-(defn fewest-cubes [results] 
- (apply merge-with max results))
+(defn fewest-cubes [results]
+  (apply merge-with max results))
 
 (fewest-cubes (list {"blue" 3, "red" 4} {"red" 1, "green" 2, "blue" 6} {"green" 2}))
 
 (defn power [results]
   (apply * (vals (fewest-cubes results))))
-  
+
 
 (power (list {"blue" 3, "red" 4} {"red" 1, "green" 2, "blue" 6} {"green" 2}))
 
@@ -116,12 +118,111 @@
   (let  [lines (s/split-lines (slurp path))
          games (map parse-game lines)]
     (apply + (for [[_ results] games]
-       (power results)))))
-    
+               (power results)))))
+
 
 (day2 "day02/example.txt")
 (day2 "day02/input.txt")
 (day2-part2 "day02/example.txt")
 (day2-part2 "day02/input.txt")
 
+(defn is-digit? [ch]
+  (and ch (Character/isDigit ch)))
 
+(is-digit? \4)
+(is-digit? \a)
+
+(defn is-symbol? [ch]
+  (not (or (is-digit? ch)
+           (= \. ch))))
+
+(defn is-gear? [ch]
+  (= \* ch))
+
+(defn load-schematic [input]
+
+  (let  [lines (s/split-lines input)
+         lists (map-indexed
+                (fn [y line]
+                  (map-indexed
+                   (fn [x char]
+                     {[x y] char})
+                   line))
+                lines)]
+    (apply merge (apply concat lists))))
+
+
+(defn symbol-positions [schematic]
+  (filter (comp is-symbol? val) schematic))
+
+(defn gear-positions [schematic]
+  (filter (comp is-gear? val) schematic))
+
+(def neighbours
+  [[-1 -1] [0 -1] [1 -1]
+   [-1  0]         [1  0]
+   [-1  1] [0  1] [1  1]])
+
+(defn numbers-beside [schematic [x y]]
+  (filter identity
+          (for [[nx ny] neighbours]
+            (let [coord [(+ nx x) (+ ny y)]
+                  cell (get schematic coord)]
+              (when (is-digit? cell)
+                coord)))))
+
+
+(def sc (load-schematic (slurp "day03/example.txt")))
+
+(pprint sc)
+
+
+(numbers-beside sc [3 1])
+
+(defn load-number-at [schematic [x y]]
+  (if (is-digit? (get schematic [(dec x) y]))
+    (load-number-at schematic [(dec x) y])
+    (let [digits (loop [i x
+                        digits []]
+                   (let [curr (get schematic [i y])]
+                     (if (is-digit? curr)
+                       (recur (inc i) (conj digits curr))
+                       {:digits digits
+                        :value (parse-long (apply str digits))
+                        :coord [i y]})))]
+      digits)))
+
+
+(load-number-at sc [3 0])
+
+(defn day3 [path]
+  (let [schematic (load-schematic (slurp path))
+        symbols (symbol-positions schematic)
+        number-maps (for [[coord] symbols]
+                      (let [nums (numbers-beside schematic coord)]
+                        (map (partial load-number-at schematic) nums)))]
+    (apply + (map :value (into #{} (apply concat number-maps))))))
+
+
+(defn gear-ratio [number-maps]
+  (when (= 2 (count number-maps))
+    (apply * (map :value number-maps))))
+
+(defn day3-part2 [path]
+  (let [schematic (load-schematic (slurp path))
+        symbols (gear-positions schematic)
+        number-maps (for [[coord] symbols]
+                      (let [nums (numbers-beside schematic coord)
+                            digits-with-dupes (map (partial load-number-at schematic) nums)]
+                        (into #{} digits-with-dupes)))]
+    (->> number-maps
+         (map gear-ratio)
+         (filter identity)
+         (apply +))))
+
+
+(day3 "day03/example.txt")
+(day3 "day03/input.txt")
+
+(day3-part2 "day03/example.txt")
+(day3-part2 "day03/input.txt")
