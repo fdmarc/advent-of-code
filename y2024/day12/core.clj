21ccdf4d04fb6679378daede7d2fda597e454543
Marc O'Morain <marc@flipdish.com>
2024-12-16 21:39:26 +0000

    Day 12

---
+++ y2024/day12/core.clj
@@ -0,0 +1,14 @@
+(ns y2024.day12.core
+  (:require [clojure.string :as s]))
+
+
+(defn parse-1 [path]
+
+  (slurp path))
+
+
+(defn solve-1 [path]
+  (parse-1 path))
+
+
+(solve-1 "y2024/day12/example1.txt")
